2cf3f8c25d747a4a7e91b10beaeadcd20228dae6
+KZ <60852359+M0REKZ@users.noreply.github.com>
2026-07-25 17:27:30 -0400

    update SyobonKZ

--- SyobonKZ/SyobonKZ.js
+++ SyobonKZ/SyobonKZ.js
@@ -71,7 +71,7 @@ var ENVIRONMENT_IS_SHELL = !ENVIRONMENT_IS_WEB && !ENVIRONMENT_IS_NODE && !ENVIR
 
 // --pre-jses are emitted after the Module integration code, so that they can
 // refer to Module (if they choose; they can also define Module)
-// include: /var/folders/dm/cwds72jn0kvd_bmcsvqsk4nh0000gp/T/tmpv6x94dxc.js
+// include: /var/folders/dm/cwds72jn0kvd_bmcsvqsk4nh0000gp/T/tmpl7n2zkx4.js
 
   if (!Module['expectedDataFileDownloads']) Module['expectedDataFileDownloads'] = 0;
   Module['expectedDataFileDownloads']++;
@@ -214,21 +214,21 @@ Module['FS_createPath']("/text", "kaizosyobon", true, true);
 
   })();
 
-// end include: /var/folders/dm/cwds72jn0kvd_bmcsvqsk4nh0000gp/T/tmpv6x94dxc.js
-// include: /var/folders/dm/cwds72jn0kvd_bmcsvqsk4nh0000gp/T/tmpdvgmiim3.js
+// end include: /var/folders/dm/cwds72jn0kvd_bmcsvqsk4nh0000gp/T/tmpl7n2zkx4.js
+// include: /var/folders/dm/cwds72jn0kvd_bmcsvqsk4nh0000gp/T/tmpfs0a5crb.js
 
     // All the pre-js content up to here must remain later on, we need to run
     // it.
     if ((typeof ENVIRONMENT_IS_WASM_WORKER != 'undefined' && ENVIRONMENT_IS_WASM_WORKER) || (typeof ENVIRONMENT_IS_PTHREAD != 'undefined' && ENVIRONMENT_IS_PTHREAD) || (typeof ENVIRONMENT_IS_AUDIO_WORKLET != 'undefined' && ENVIRONMENT_IS_AUDIO_WORKLET)) Module['preRun'] = [];
     var necessaryPreJSTasks = Module['preRun'].slice();
-  // end include: /var/folders/dm/cwds72jn0kvd_bmcsvqsk4nh0000gp/T/tmpdvgmiim3.js
-// include: /var/folders/dm/cwds72jn0kvd_bmcsvqsk4nh0000gp/T/tmpz4duin4p.js
+  // end include: /var/folders/dm/cwds72jn0kvd_bmcsvqsk4nh0000gp/T/tmpfs0a5crb.js
+// include: /var/folders/dm/cwds72jn0kvd_bmcsvqsk4nh0000gp/T/tmphj_jpir5.js
 
     if (!Module['preRun']) throw 'Module.preRun should exist because file support used it; did a pre-js delete it?';
     necessaryPreJSTasks.forEach((task) => {
       if (Module['preRun'].indexOf(task) < 0) throw 'All preRun tasks that exist before user pre-js code should remain after; did you replace Module or modify Module.preRun?';
     });
-  // end include: /var/folders/dm/cwds72jn0kvd_bmcsvqsk4nh0000gp/T/tmpz4duin4p.js
+  // end include: /var/folders/dm/cwds72jn0kvd_bmcsvqsk4nh0000gp/T/tmphj_jpir5.js
 
 
 var programArgs = [];
